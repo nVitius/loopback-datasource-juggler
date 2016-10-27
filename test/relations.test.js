a9dac45cab2ae20c04919a124179f8722f826c29
jannyHou <juehou@ca.ibm.com>
2016-10-27 10:18:54 -0400

    Fix test case

--- test/relations.test.js
+++ test/relations.test.js
@@ -738,11 +738,13 @@ describe('relations', function() {
             ch1.should.have.lengthOf(1);
             ch1[0].name.should.eql('z');
             //order
-            physician.patients({order: 'patientId DESC'}, function(err2, ch2) {
+            //should apply on related model instead of through model 
+            physician.patients({order: 'name DESC'}, function(err2, ch2) {
               should.not.exist(err2);
               should.exist(ch2);
+              console.log(ch2)
               ch2.should.have.lengthOf(3);
-              ch2[0].name.should.eql('c');
+              ch2[0].name.should.eql('z');
               done();
             });
           });
